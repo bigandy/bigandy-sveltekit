8e67f1390a0dde15235539448aab14eea1c486cb
Andrew Hudson <ahudson@gmail.com>
2024-02-16 14:39:55 +0000

    remove slot, add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
 <!-- +layout.svelte -->
 <script>
+	import "@fontsource/inter/900.css"; 
+	import "@fontsource/inter/700.css"; 
+	import "@fontsource/inter"; // this is the 400 weight 
 	import Header from '$components/Header.svelte';
 	import '$styles/style.scss';
+
+	const { children } = $props();
 </script>
 
 <Header />
 
 <main>
-	<slot />
+	{@render children()}
 </main>
 
 <footer>&copy; Andrew JD Hudson</footer>
